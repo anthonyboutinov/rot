0ed945d823e0f8b7657c19d8573b2893d9ce516d
Anthony Boutinov <anton4488@gmail.com>
2016-05-21 10:35:51 +0300

    init web-app

---
+++ web-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rotApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rotApp
+ */
+angular.module('rotApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

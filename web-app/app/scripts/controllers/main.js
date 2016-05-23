234c3021a14d89feaa3f8ce602e33478aaf70b25
Anthony Boutinov <anton4488@gmail.com>
2016-05-23 13:38:35 +0300

    (invalid) call to web service
    $http get call returns:
    <h1>JavaCodeEvaluator</h1> <p>Hi there, this is an AXIS service!</p>
    <i>Perhaps there will be a form for invoking the service here...</i>

--- web-app/app/scripts/controllers/main.js
+++ web-app/app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the rotApp
  */
 angular.module('rotApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
 
     $scope.editorOptions = {
       lineWrapping : true,
@@ -20,10 +20,70 @@ angular.module('rotApp')
       autofocus: true,
     };
 
-    $scope.code = '<?php\necho "Hello World";\n?>';
     $scope.console = {
       output: 'Здесь будут отображаться результаты работы программы',
     };
+
+    $scope.langs = [
+      {
+        name: "PHP",
+        mode: "php",
+        handler: function(input) {},
+        sampleCode: '<?php\necho "Hello World";\n?>',
+      },
+      {
+        name: "Java",
+        mode: "text/x-java",
+        handler: function(input) {
+
+          var address = "http://localhost:8080/rot-java-webservice/services/JavaCodeEvaluator";
+
+          $http.get(address).
+            success(function(data) {
+              console.log(data);
+              $scope.console.output = data;
+            });
+
+        },
+        sampleCode: 'System.out.println("Hello World");',
+      },
+      {
+        name: "C++",
+        mode: "text/x-c++src",
+        handler: function(input) {},
+        sampleCode: '// No sample code set for C++'
+      },
+    ];
+
+    $scope.currentLang = "PHP";
+
+    var setSampleCode = function() {
+      for (var i = 0; i < $scope.langs.length; i++) {
+        if ($scope.langs[i].name === $scope.currentLang) {
+          $scope.code = $scope.langs[i].sampleCode;
+          $scope.editorOptions.mode = $scope.langs[i].mode;
+        }
+      }
+    }
+
+    setSampleCode();
+
+    $scope.setLang = function(langName) {
+      $scope.currentLang = langName;
+      setSampleCode();
+    }
+
+    $scope.run = function() {
+      if ($scope.code === "") {
+        $scope.console = "Невозможно запустить: нет кода";
+      } else {
+        for (var i = 0; i < $scope.langs.length; i++) {
+          if ($scope.langs[i].name === $scope.currentLang) {
+            $scope.langs[i].handler($scope.code);
+          }
+        }
+      }
+    }
 
 
   });

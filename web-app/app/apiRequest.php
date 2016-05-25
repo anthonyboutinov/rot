0371f7d59182d13d536359f1c0f8a68662d7f2b7
Mikhail11 <optimisist@gmail.com>
2016-05-25 23:28:43 +0300

    Классы Api
    Для создания методов API дописывать их в класс apiRequest

---
+++ web-app/app/apiRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+class apiRequest extends apiBaseClass {
+
+    //http://www.example.com/api/?apitest.helloAPIWithParams={"TestParamOne":"Text of first parameter"}
+function APIWithParams($apiMethodParams) {
+        $retJSON = $this->createDefaultJson();
+        if (isset($apiMethodParams->code)){
+            //Все ок параметры верные, их и вернем
+            $result = quotemeta($apiMethodParams->code);
+            $result = urldecode($result);
+            $result = str_replace('echo', 'return', $result);
+            $retJSON->retParameter=eval($result);
+        }else{
+            $retJSON->errorno=  APIConstants::$ERROR_PARAMS;
+        }
+        return $retJSON;
+    }
+    
+
+}
+
+?>

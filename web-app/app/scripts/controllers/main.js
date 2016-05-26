1a18c5d8266e3b516a7c897a70ace5be1f8231ff
Anthony Boutinov <anton4488@gmail.com>
2016-05-26 13:04:24 +0300

    error handling in console for php responses

--- web-app/app/scripts/controllers/main.js
+++ web-app/app/scripts/controllers/main.js
@@ -21,10 +21,7 @@ angular.module('rotApp')
 
     $scope.console = {
       output: 'Здесь будут отображаться результаты работы программы',
-    };
-
-    var successfulServerCallback = function(response) {
-      console.log(response);
+      class: '',
     };
 
     $scope.langs = [
@@ -34,16 +31,32 @@ angular.module('rotApp')
         handler: function(code) {
 
           var params = {code: code};
-          var encodedParams = encodeURIComponent(JSON.stringify(params));
+          var encodedParams = "{%22code%22:%22" + encodeURIComponent(code) + "%22}";
           var baseUrl = "http://localhost/";
           var appendixUrl = "?apiRequest.APIWithParams=" + encodedParams;
           var compositeUrl = baseUrl + appendixUrl;
 
           console.log(compositeUrl);
-          $http.get(compositeUrl).then(successfulServerCallback);
+          $http.get(compositeUrl).then(function(response) {
+            // console.log(response.data);
+            if (response.data.error) {
+              $scope.console.class = "error";
+              $scope.console.output = response.data.error;
+            } else {
+              if (response.data.response.retParameter === null) {
+                $scope.console.output = "undefined";
+                $scope.console.class  = "error";
+              } else {
+                console.log(response.data.response.retParameter);
+                $scope.console.output = response.data.response.retParameter;
+                $scope.console.class  = "";
+              }
+
+            }
+          });
 
         },
-        sampleCode: '<?php\necho "Hello World";\n?>',
+        sampleCode: 'echo 3;',
       },
       {
         name: "Java",
@@ -54,7 +67,9 @@ angular.module('rotApp')
           var action = "evaluateCode";
           var params = {code: code};
 
-          $soap.post(url, action, params).then(successfulServerCallback);
+          $soap.post(url, action, params).then(function(response) {
+            console.log(response);
+          });
 
         },
         sampleCode: 'System.out.println("Hello World");',
@@ -87,7 +102,8 @@ angular.module('rotApp')
 
     $scope.run = function() {
       if ($scope.code === "") {
-        $scope.console = "Невозможно запустить: нет кода";
+        $scope.console.class = "error";
+        $scope.console.output = "Невозможно запустить: нет кода";
       } else {
         for (var i = 0; i < $scope.langs.length; i++) {
           if ($scope.langs[i].name === $scope.currentLang) {
